5ab3f3e2e496220cb6033e08c905b9b9d9cc45e4
Alexander Nasonov <alnsn@yandex.ru>
2015-08-04 22:02:50 +0100

    Use g:core_size() and g:edge(at).

--- examples/test-dict.lua
+++ examples/test-dict.lua
@@ -7,4 +7,15 @@ local seed  = tonumber(args[2] or "0")
 local f = io.open("/usr/share/dict/words")
 local g = rgph.new_graph(nkeys, "jenkins2")
 print(g:entries(), g:vertices())
-print(g:build(seed, f:lines()))
+
+local ok,err = g:build(seed, f:lines())
+print(ok, err)
+
+if not ok and not err then
+	print("core", g:core_size())
+end
+
+if not err then
+	print(g:edge(1))
+	print(g:edge(nkeys))
+end
