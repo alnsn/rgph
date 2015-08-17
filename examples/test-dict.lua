3cdcd8c447cb6c1930300dba7d833e248e14fa1b
Alexander Nasonov <alnsn@yandex.ru>
2015-08-17 08:21:39 +0100

    Open any file and print duplicates.

--- examples/test-dict.lua
+++ examples/test-dict.lua
@@ -3,8 +3,9 @@ local rgph = require "rgph"
 local args = { ... }
 local nkeys = tonumber(args[1] or "1")
 local seed  = tonumber(args[2] or "0")
+local file  = args[3] or "/usr/share/dict/words"
 
-local f = io.open("/usr/share/dict/words")
+local f = io.open(file)
 local g = rgph.new_graph(nkeys, "jenkins2")
 print(g:entries(), g:vertices())
 
@@ -12,7 +13,8 @@ local ok,err = g:build(seed, f:lines())
 print(ok, err)
 
 if not ok and not err then
-	print("core", g:core_size())
+	local f = io.open(file)
+	print("duplicates", g:find_duplicates(f:lines()))
 end
 
 if not err then
