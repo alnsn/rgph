9b202267ee2ad5d11e4a95cce9e2858c3a79c345
Alexander Nasonov <alnsn@yandex.ru>
2015-08-22 10:24:27 +0100

    Fix mixing length for 64-bit arrays and add forgotten d2u64().

--- lib/jenkins2.c
+++ lib/jenkins2.c
@@ -268,8 +268,6 @@ rgph_u32x3_jenkins2_u8a(const uint8_t * restrict key,
 			MIX(h[0], h[1], h[2]);
 		}
 
-		h[2] += len;
-
 		if (end - key >= 4) {
 			h[n++] += htole32(*((const uint32_t *)&key[0]));
 			key += 4;
@@ -285,8 +283,6 @@ rgph_u32x3_jenkins2_u8a(const uint8_t * restrict key,
 			h[2] += read32(&key[8]);
 			MIX(h[0], h[1], h[2]);
 		}
-
-		h[2] += len;
 
 		if (end - key >= 4) {
 			h[n++] += read32(&key[0]);
@@ -306,6 +302,7 @@ rgph_u32x3_jenkins2_u8a(const uint8_t * restrict key,
 	case 1:
 		h[n] += (uint32_t)key[0];       /* FALLTHROUGH */
 	case 0:
+		h[2] += len;
 		MIX(h[0], h[1], h[2]);
 	}
 }
@@ -492,7 +489,7 @@ rgph_u32x3_jenkins2_f64a(const double * restrict key,
 		h[0] += b >> 32;
 		break;
 	case 1:
-		a = htole64(key[0]);
+		a = htole64(d2u64(key[0]));
 		h[0] += a & UINT32_MAX;
 		h[1] += a >> 32;
 		break;
