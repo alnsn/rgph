271559b7069c45d27c8445f55796671a83cf1d0d
Alexander Nasonov <alnsn@yandex.ru>
2016-01-20 19:42:02 +0000

    Implement rgph_u32x4_murmur32_data{32,64} functions.

--- lib/murmur32.c
+++ lib/murmur32.c
@@ -263,50 +263,99 @@ rgph_u32x4_murmur32_data(const void *data,
 
 	w[0] = w[1] = w[2] = w[3] = 0;
 
-	switch ((end - key) & 15) {
+	switch (end - key) {
 	case 15: w[3] ^= key[14] << 16;
 	case 14: w[3] ^= key[13] << 8;
 	case 13: w[3] ^= key[12];
 	         rgph_murmur32_mix3(w[3], h);
-		 /* FALLTHROUGH */
+	         /* FALLTHROUGH */
 	case 12: w[2] ^= key[11] << 24;
 	case 11: w[2] ^= key[10] << 16;
 	case 10: w[2] ^= key[9] << 8;
 	case  9: w[2] ^= key[8];
 	         rgph_murmur32_mix2(w[2], h);
-		 /* FALLTHROUGH */
+	         /* FALLTHROUGH */
 	case  8: w[1] ^= key[7] << 24;
 	case  7: w[1] ^= key[6] << 16;
 	case  6: w[1] ^= key[5] << 8;
 	case  5: w[1] ^= key[4];
-	         rgph_murmur32_mix1(w[1], h);
-		 /* FALLTHROUGH */
+	      rgph_murmur32_mix1(w[1], h);
+	      /* FALLTHROUGH */
 	case  4: w[0] ^= key[3] << 24;
 	case  3: w[0] ^= key[2] << 16;
 	case  2: w[0] ^= key[1] << 8;
 	case  1: w[0] ^= key[0];
 	         rgph_murmur32_mix0(w[0], h);
-	};
-
-	rgph_murmur32_finalise(len, h);
+	         /* FALLTHROUGH */
+	case 0:
+	         rgph_murmur32_finalise(len, h);
+	}
 }
 
 inline void
 rgph_u32x4_murmur32_data32(const void *data,
-    size_t len, uint32_t seed, uint32_t *h)
+    size_t len, uint32_t seed, uint32_t * restrict h)
 {
+	const uint32_t * restrict key = data;
+	const uint32_t *end = key + len;
+	uint32_t w[4];
 
-	/* XXX implement */
-	return rgph_u32x4_murmur32_data(data, len * sizeof(uint32_t), seed, h);
+	h[0] = h[1] = h[2] = h[3] = seed;
+
+	for (; end - key >= 4; key += 4) {
+		w[0] = htole32(key[0]);
+		w[1] = htole32(key[1]);
+		w[2] = htole32(key[2]);
+		w[3] = htole32(key[3]);
+		rgph_murmur32_mix(w, h);
+	}
+
+	switch (end - key) {
+	case 3: w[2] = htole32(key[2]);
+	        rgph_murmur32_mix2(w[2], h);
+	        /* FALLTHROUGH */
+	case 2: w[1] = htole32(key[1]);
+	        rgph_murmur32_mix1(w[1], h);
+	        /* FALLTHROUGH */
+	case 1: w[0] = htole32(key[0]);
+	        rgph_murmur32_mix0(w[0], h);
+	        /* FALLTHROUGH */
+	case 0:
+		rgph_murmur32_finalise(len * sizeof(key[0]), h);
+	}
 }
 
 inline void
 rgph_u32x4_murmur32_data64(const void *data,
-    size_t len, uint32_t seed, uint32_t *h)
+    size_t len, uint32_t seed, uint32_t * restrict h)
 {
+	const uint64_t * restrict key = data;
+	const uint64_t *end = key + len;
+	uint32_t w[4];
+	uint64_t a, b;
 
-	/* XXX implement */
-	return rgph_u32x4_murmur32_data(data, len * sizeof(uint64_t), seed, h);
+	h[0] = h[1] = h[2] = h[3] = seed;
+
+	for (; end - key >= 2; key += 2) {
+		a = htole64(key[0]);
+		b = htole64(key[1]);
+		w[0] = a & UINT32_MAX;
+		w[1] = a >> 32;
+		w[2] = b & UINT32_MAX;
+		w[3] = b >> 32;
+		rgph_murmur32_mix(w, h);
+	}
+
+	switch (end - key) {
+	case 1: a = htole64(key[0]);
+	        w[0] = a & UINT32_MAX;
+	        w[1] = a >> 32;
+	        rgph_murmur32_mix1(w[1], h);
+	        rgph_murmur32_mix0(w[0], h);
+	        /* FALLTHROUGH */
+	case 0:
+	        rgph_murmur32_finalise(len * sizeof(key[0]), h);
+	}
 }
 
 #if defined(WEAK_ALIASES) && !defined(__STRICT_ANSI__)
