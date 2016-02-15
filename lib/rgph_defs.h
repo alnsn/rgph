01e457577ab75af8716ca9a9f50cd3b0acc62c32
Alexander Nasonov <alnsn@yandex.ru>
2016-02-15 23:33:16 +0000

    Copyright year 2016.

--- lib/rgph_defs.h
+++ lib/rgph_defs.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2015 Alexander Nasonov.
+ * Copyright (c) 2015-2016 Alexander Nasonov.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
