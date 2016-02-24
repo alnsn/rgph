8bc21d5af8233e9177dd3f4c9ea6731daf9d237c
Alexander Nasonov <alnsn@yandex.ru>
2016-02-24 23:47:13 +0000

    Adapt to new g:edges() and small changes.

--- examples/rgphdot.lua
+++ examples/rgphdot.lua
@@ -4,15 +4,18 @@ local function dot(file, g, ...)
 	local iter = select(1, ...) ~= nil
 
 	file:write("graph {\n")
-	for h1, h2, peel, key in g:edges("peel", ...) do
+	local edge_fmt = "%d -- %d [label=%q, color=%q];\n"
+	for peel, key, h1, h2, h3 in g:edges("peel,key", ...) do
 		local color = peel > 0 and "black" or "red"
-		local label = string.format("%d/%s", peel, key)
-		file:write(string.format("%d -- %d [label=%q, color=%q];\n",
-		    h1, h2, label, color))
+		local label = string.format("%s", key)
+		file:write(edge_fmt:format(h1, h2, label, color))
+		if h3 then
+			file:write(edge_fmt:format(h2, h3, label, color))
+		end
 	end
-	local label = string.format("%d edges, %d vertices, seed %d",
-	    g:entries(), g:vertices(), g:seed())
+	local label = string.format("%d edges, %d vertices, core %d, seed %d",
+	    g:entries(), g:vertices(), g:core_size(), g:seed())
 	file:write(string.format("label=%q;}\n", label))
 end
 
-return dot
+return { dot=dot }
