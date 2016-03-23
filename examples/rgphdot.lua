f0b607d168f0714ff58cce0097764c9ebf3105cd
Alexander Nasonov <alnsn@yandex.ru>
2016-03-23 19:55:24 +0000

    Minor changes.

--- examples/rgphdot.lua
+++ examples/rgphdot.lua
@@ -1,21 +1,26 @@
 local rgph = require "rgph"
 
+local edge_fmt = "%d -- %d [label=%q, color=%q];\n"
+local graph_label_fmt = "%d edges, %d vertices, core %d, seed %d, flags %q"
+
 local function dot(file, g, ...)
-	local iter = select(1, ...) ~= nil
+	local seed = g:seed()
+	--local hash = rgph[g:hash()]
 
 	file:write("graph {\n")
-	local edge_fmt = "%d -- %d [label=%q, color=%q];\n"
+
 	for peel, key, h1, h2, h3 in g:edges("peel,key", ...) do
 		local color = peel > 0 and "black" or "red"
 		local label = string.format("%s", key)
+	--	local label = string.format("%s %x %x %x", key, hash(key, seed))
 		file:write(edge_fmt:format(h1, h2, label, color))
 		if h3 then
 			file:write(edge_fmt:format(h2, h3, label, color))
 		end
 	end
-	local label_fmt = "%d edges, %d vertices, core %d, seed %d, flags %q"
-	local label = label_fmt:format(g:entries(),
-	    g:vertices(), g:core_size(), g:seed(), g:flags())
+
+	local label = graph_label_fmt:format(g:entries(),
+	    g:vertices(), g:core_size(), seed, g:flags())
 	file:write(string.format("label=%q;}\n", label))
 end
 
