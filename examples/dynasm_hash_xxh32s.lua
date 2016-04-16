064ba488aa2c96702377d59853bc00308b0b61e7
Alexander Nasonov <alnsn@yandex.ru>
2016-04-16 14:23:09 +0100

    Switch to FourCC.

--- examples/dynasm_hash_xxh32s.lua
+++ examples/dynasm_hash_xxh32s.lua
@@ -7,26 +7,32 @@ local args = { ... }
 local ntries = 20
 local seed = tonumber(args[1] or 0xbb8bb8d2)
 local keys = {
-	["\0\0\0\0"] = "zero",
-	["\1\0\0\0"] = "one",
-	["\2\0\0\0"] = "two",
-	["\3\0\0\0"] = "three",
-	["\4\0\0\0"] = "four",
-	["\5\0\0\0"] = "five",
-	["\6\0\0\0"] = "six",
-	["\7\0\0\0"] = "seven",
-	["\8\0\0\0"] = "eight",
-	["\9\0\0\0"] = "nine"
+	-- FourCC codes
+	AIFF = "unused",
+	DIVX = "unused",
+	GEOX = "unused",
+	H264 = "unused",
+	I420 = "unused",
+	MRLE = "unused",
+	MSVC = "unused",
+	RIFF = "unused",
+	SVID = "unused",
+	XDIV = "unused"
 }
 
 local code, buf, size = hash.generate(keys,
     "xxh32s,bdz,rank3,pow2", ntries, seed)
 dasm.dump(buf, size)
 
+local function fourCC(str)
+	local a,b,c,d = str:byte(1, 4)
+	return d * 16777216 + c * 65536 + b * 256 + a
+end
+
 local hashes = {}
 local hash_hits = {}
 for key,_ in pairs(keys) do
-	local i = string.byte(key:sub(1,1))
+	local i = fourCC(key)
 	local h = code(i)
 	if hash_hits[h] then
 		error(string.format("duplicate hash, seed=%d", seed))
